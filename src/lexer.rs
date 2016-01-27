fe15be00fe3073d82eaa2aae2a427e89e795cf0e
Antonio <a.carta@outlook.it>
2016-01-27 18:16:07 +0100

    Scanner works reading from standard input. Needs some unit tests and parsing procedure for every token.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,33 +1,37 @@
 use std::collections::HashMap;
 use std::io;
+use std::io::{Read, Stdin};
+use std::string::String;
+use std::string;
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 enum Boolop {
     Or,
     And
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 enum Relop {
     Ge, Gr,
     Leq, les,
     Equ, Neq,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 enum Unary {
     Minus,
     Not,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 enum Numop {
     Add, Sub,
     Mul, Div,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 pub enum Token {
+    Eof, // Finished.
     /* Reserved words. */
     If,
     Else,
@@ -56,23 +60,29 @@ pub struct Scanner {
         This way we don't end up we thousands of If and While tokens.
     */
     createdTokens : HashMap<Token, Token>,
+    buffer : String,
     lookahead : char,
 }
 
 impl Scanner {
     pub fn new() -> Scanner {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s);
+        println!("{}", s);
         Scanner {
             createdTokens : HashMap::new(),
+            buffer : s,
             lookahead : ' ',
         }
     }
 
     /* Scan the input until it finds a token. */
-    pub fn scan(&self) -> Token {
+    pub fn scan(&mut self) -> Token {
         match self.lookahead {
+            '\0' => Token::Eof,
             ' '|'\t'|'\n' => {
                 /* Skip spaces. Read new character. */
-                self.lookahead = read_char();
+                self.lookahead = self.read_char();
                 self.scan()
             },
             '0' ... '9' => self.scan_number(),
@@ -80,58 +90,64 @@ impl Scanner {
             'A' ... 'Z' => self.scan_iden_keyword(),
             '='|'!'|'<'|'>' => self.scan_relop(),
             '|'|'&' => self.scan_boolop(),
-            '+' => Token::NumOP(Numop::Add),
-            '-' => Token::NumOP(Numop::Sub),
-            '*' => Token::NumOP(Numop::Mul),
-            '/' => Token::NumOP(Numop::Div),
-            '(' => Token::LParen,
-            ')' => Token::RParen,
-            '[' => Token::LArrParen,
-            ']' => Token::RArrParen,
-            '{' => Token::OpenBlock,
-            '}' => Token::CloseBlock,
-            ';' => Token::SemiColon,
-            _ => Token::If, /* ERRORE!!! */
+            '+' => self.single_token(Token::NumOP(Numop::Add)),
+            '-' => self.single_token(Token::NumOP(Numop::Sub)),
+            '*' => self.single_token(Token::NumOP(Numop::Mul)),
+            '/' => self.single_token(Token::NumOP(Numop::Div)),
+            '(' => self.single_token(Token::LParen),
+            ')' => self.single_token(Token::RParen),
+            '[' => self.single_token(Token::LArrParen),
+            ']' => self.single_token(Token::RArrParen),
+            '{' => self.single_token(Token::OpenBlock),
+            '}' => self.single_token(Token::CloseBlock),
+            ';' => self.single_token(Token::SemiColon),
+            _ => Token::Eof, /* ERRORE!!! */
         }
     }
 
-    fn gen_token(&self, tok : Token) -> &Token {
-        self.createdTokens.get(&tok)
-            .unwrap_or(&tok)
+    fn single_token(&mut self, tok : Token) -> Token {
+        self.lookahead = ' ';
+        tok
     }
 
-    fn scan_number(&self) -> Token {
-        let v = 0;
+    fn scan_number(&mut self) -> Token {
+        let mut v = 0;
         while self.lookahead.is_digit(10) {
             v = v*10;
             v += self.lookahead.to_digit(10).unwrap();
-            self.lookahead = read_char();
+            self.lookahead = self.read_char();
         }
         /* TODO: check for float. */
-        Token::Num(v)
+        Token::Num(0)
     }
 
-    fn scan_iden_keyword(&self) -> Token {
-        let keywords = {"if"; "else"; "while"; "break"};
-        let s = String::new();
+    fn scan_iden_keyword(&mut self) -> Token {
+        //let keywords = {"if"; "else"; "while"; "break"};
+        let mut s = String::new();
         while self.lookahead.is_alphanumeric() {
             s.push(self.lookahead);
-            self.lookahead = read_char();
+            self.lookahead = self.read_char();
         }
         /* TODO: check for keywords. */
         Token::Ide(s)
     }
 
-    fn scan_relop(&self) -> Token {
+    fn scan_relop(&mut self) -> Token {
         // '='|'!'|'<'|'>'
+        self.lookahead = ' '; // match token.
         Token::LParen
     }
-    fn scan_boolop(&self) -> Token {
+    fn scan_boolop(&mut self) -> Token {
         // '|'|'&'
+        self.lookahead = ' '; // match token.
         Token::LParen
     }
-}
 
-fn read_char() -> char {
-    'a'
+    fn read_char(&mut self) -> char {
+        if self.buffer.len() == 0 {
+            '\0'
+        } else {
+            self.buffer.remove(0)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::fmt::{self, Debug, Display};
+use std::io;
+
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
-    let scanner = lexer::Scanner::new();
-    scanner.scan();
+/*
+    let buf = String::new();
+    io::stdin().read_to_string(buf);
+
+    let s = "asdasas";
+    for c in s.chars() {
+        println!("{}", c);
+    }
+*/
+    println!("Mini Compiler.");
+    let mut scanner = lexer::Scanner::new();
+    let mut tok = scanner.scan();
+    while tok != lexer::Token::Eof {
+        println!("{:?}", tok);
+        tok = scanner.scan();
+    }
 }
